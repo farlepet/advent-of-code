25d242277bed2ac31ecd049fab6c2ff510c8ad90
Peter Farley <far.peter1@gmail.com>
2023-12-02 18:31:17 -0600

    2023 Day 2

---
+++ 2023/2.2/main.rs
@@ -0,0 +1,126 @@
+use std::env;
+use std::fs;
+use regex::Regex;
+
+struct Cubes {
+    red:   u32,
+    green: u32,
+    blue:  u32
+}
+
+struct Game {
+    num:   u32,
+    pulls: Vec<Cubes>
+}
+
+fn min_game(game: &Game) -> Cubes {
+    let mut max = Cubes {
+        red:   game.pulls[0].red,
+        green: game.pulls[0].green,
+        blue:  game.pulls[0].blue
+    };
+    /* Repeating the first index, because lazy */
+    for pull in &game.pulls {
+        if pull.red   > max.red {
+            max.red = pull.red;
+        }
+        if pull.green > max.green {
+            max.green = pull.green;
+        }
+        if pull.blue  > max.blue {
+            max.blue = pull.blue;
+        }
+    }
+
+    return max;
+}
+
+fn parse_line(line: &str) -> Option<Game> {
+    let mut game = Game{
+        num:   0,
+        pulls: vec![]
+    };
+
+    let game_re  = Regex::new(r"Game ([0-9]*)").unwrap();
+    let game_cap = game_re.captures(line).unwrap();
+    if game_cap.len() < 2 {
+        return None;
+    }
+    game.num = game_cap[1].parse::<u32>().unwrap();
+
+    let red_re   = Regex::new(r".*?([0-9]*) red").unwrap();
+    let green_re = Regex::new(r".*?([0-9]*) green").unwrap();
+    let blue_re  = Regex::new(r".*?([0-9]*) blue").unwrap();
+
+    let mut begin  = line.find(':').unwrap() + 1;
+
+    let mut finish = false;
+
+    while !finish {
+        let substr = &line[begin..];
+
+        let mut pull = Cubes {
+            red:   0,
+            green: 0,
+            blue:  0
+        };
+
+        let _end = substr.find(';');
+        let end;
+        match _end {
+            Some(n) => {
+                begin += n + 1;
+                end    = n;
+            },
+            None    => {
+                end    = substr.len();
+                finish = true;
+            }
+        }
+        let ssubstr = &substr[..end];
+
+        if let Some(cap) = red_re.captures(ssubstr) {
+            if cap.len() > 1 {
+                pull.red = cap[1].parse::<u32>().unwrap();
+            }
+        }
+        if let Some(cap) = green_re.captures(ssubstr) {
+            if cap.len() > 1 {
+                pull.green = cap[1].parse::<u32>().unwrap();
+            }
+        }
+        if let Some(cap) = blue_re.captures(ssubstr) {
+            if cap.len() > 1 {
+                pull.blue = cap[1].parse::<u32>().unwrap();
+            }
+        }
+
+        game.pulls.push(pull);
+    }
+
+    return Some(game);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2  {
+        println!("Must pass in input file as an argument!");
+        std::process::exit(-1);
+    }
+    let filename: &String  = &args[1];
+
+    let mut sum = 0;
+
+    // Horribly inefficient
+    for line in fs::read_to_string(filename).unwrap().lines() {
+        let game  = parse_line(line).unwrap();
+        let min   = min_game(&game);
+        let power = min.red * min.blue * min.green;
+
+        sum += power;
+    }
+
+    println!("Result: {sum}");
+
+    std::process::exit(0);
+}
